9dc7658a325b63a77df3d6d6878d4383a18d7411
RobertBowen <givendifferent@gmail.com>
2023-05-07 11:13:39 -0700

    created basic project header.

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from "./components/AppHeader"
+
 
 function App() {
-  return (
-    <div className="App">
   
+  return(
+    <div class ="app">
+      <h1>hello!!</h1>
+      <Header/>
     </div>
-  );
+  )
+
 }
+
+
+
 
 export default App;
---
+++ app/src/components/AppHeader.js
@@ -0,0 +1,29 @@
+const AppHeader = () => 
+{
+
+  return(
+  <div class = "header">
+
+    <h1>Robert Bowen</h1>
+
+    <h2>CSNW Interview 2: Boggle Program</h2>
+
+    <h3>
+    Project Description
+
+    </h3>
+
+    <body>Given a 4x4 matrix of letters chosen randomly from A-Z (with replacement), 
+    and a list L of English words (you can use any words you’d like); 
+    write a program to output all the words present in the matrix per the rules of Boggle.
+    Your program should be able to accept pre-set 4x4 matrices and word lists in a reasonable (text-based) format of your choosing.
+    </body>
+
+  </div>
+  ); 
+
+
+
+}
+
+export default AppHeader
