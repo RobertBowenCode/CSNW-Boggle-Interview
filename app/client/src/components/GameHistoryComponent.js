caa4990ebf8d9d746c9b696b380d00d8201fdf37
RobertBowen <givendifferent@gmail.com>
2023-05-08 13:18:33 -0700

    creating Game history components

---
+++ app/client/src/components/GameHistoryComponent.js
@@ -0,0 +1,100 @@
+
+
+import ListComponent from './ListComponent'
+import '../style/Game.css';
+
+const GameHistoryComponent = ({my_key, matrix, words, found_words}) => {
+
+    let not_found_words = words
+
+    found_words?.forEach(
+
+       (word) =>
+       {
+           not_found_words = not_found_words.filter((e) => e!== word);
+
+       }
+
+
+    )
+
+
+
+    const not_found_list = not_found_words.map( (word, key) =>//create list of potential words
+    <ListComponent 
+     word = {word}
+     onRemove= {null}
+     key = {key}
+    />)
+
+    const found_list =  found_words.map( (word, key) =>//create list of potential words
+    <ListComponent 
+     word = {word}
+     onRemove= {null}
+     key = {key}
+    />)
+
+
+
+
+
+
+
+
+
+
+    return(
+        <div className="Board" key = {my_key}>
+        <h3>
+            Word Board
+        </h3>
+        <table>
+            <tbody>
+            {matrix.map((row, rowIndex) => (
+                <tr key={rowIndex}>
+                    {row.map((column, columnIndex) => (
+                        <td key={columnIndex}>
+                        {matrix[rowIndex][columnIndex] == null ? '' : matrix[rowIndex][columnIndex]  }
+                        </td>
+                    ))}
+                </tr>
+                ))}
+            </tbody>
+        </table>
+
+        <div className="caption" key = {my_key}>
+        </div>
+
+
+
+        <div key = {my_key}>
+        <div key = {my_key}>
+            <h3 className='found'>
+                Found Words
+            </h3>
+            <ul>{found_list}</ul>
+            
+
+        </div>
+
+        <div   key = {my_key}>
+            <h3 className='not_found'>
+                Missed Words
+            </h3>
+            <ul> {not_found_list} </ul>
+            
+
+        </div>
+       
+
+
+    </div>
+    </div>
+       
+
+       
+
+    )
+}
+
+export default GameHistoryComponent
--- app/client/src/components/History.js
+++ app/client/src/components/History.js
@@ -1,17 +1,30 @@
 
+import GameHistoryComponent from './GameHistoryComponent'
+
 
 export default function History(current_history){
 
 
 console.log(current_history)
 
+const listItems = current_history.map( (history, key) =>//create list of potential words
+<GameHistoryComponent 
+ matrix = {history["board"]}
+ words= {history["words"]}
+ found_words = {history["found_words"]}
+ my_key = {key}
+/>
+
+
+); 
 
     
 
 return(
 
     <>
-        History
+        <h3> Game History</h3>
+        {listItems}
     </>
 )
 
